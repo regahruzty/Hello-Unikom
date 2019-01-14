1a13ac162683028c8a64b4c3c7f2dcb112ec22cf
herdi <dfikr94@gmail.com>
2019-01-14 13:26:37 +0700

    Coba

---
+++ src/main/java/aplikasi/codeshare/herdi/model/Migrate.java
@@ -0,0 +1,20 @@
+package main.java.aplikasi.codeshare.herdi.model;
+
+public class Migrate {
+
+    public static void main(String[] args) {
+
+    }
+
+    class Utils{
+        private static final String DB_URL =
+                "jdbc:mysql://localhost:3306/belajar_jdbc";
+        private static final String USER =
+                "root";
+        private static final String PASSWORD =
+                "root";
+
+    }
+
+
+}
--- src/main/java/aplikasi/practice/TransactionToJDBC.java
+++ src/main/java/aplikasi/practice/TransactionToJDBC.java
@@ -1,46 +1,76 @@
 package main.java.aplikasi.practice;
 
+import com.mysql.jdbc.Driver;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TransactionToJDBC {
 
-    private static final String DB_URL =
-            "jdbc:mysql://localhost:3306/belajar_jdbc";
-    private static final String USER =
-            "root";
-    private static final String PASSWORD =
-            "root";
-//    private static final String DRIVER =
-//            "com.mysql.cj.jdbc.Driverajsdfkljas;dlfj;";
-
     public static void main(String[] args) {
-        Connection conn = null;
-        Statement statement = null;
-        System.out.println("INI DB URL SAYA : "+DB_URL);
-        System.out.println("INI USER DB SAYA : "+USER);
-        System.out.println("INI PASS DB SAYA : "+PASSWORD);
-//        System.out.println("INI DRIVER JDBC SAYA : "+DRIVER);
-        try {
-            conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-            statement = conn.createStatement();
-            String sql = " CREATE TABLE table_satu ( " +
-                    "   id_table_satu INT(11) not null PRIMARY KEY auto_increment, " +
-                    "   description VARCHAR(255) not null, " +
-                    "   create_user VARCHAR(255) not null, " +
-                    "   create_date DATE not null " +
-                    "   )";
-            statement.executeUpdate(sql);
-            System.out.println("INI CONNECTION SAYA : "+conn);
-            System.out.println("CREATE TABLE SUCCESS ");
-        } catch (SQLException e){
-            System.out.println("GET CONNECTION FAILED");
-            System.out.println("CREATE TABLE FAILED ");
-            e.printStackTrace();
-        }
-
+     //Utils.createTableSatu();
+     Utils.createTableKaryawan();
     }
 
-}
+    static class Utils {
+
+            private static final String DB_URL =
+                    "jdbc:mysql://localhost:3306/belajar_jdbc";
+            private static final String USER =
+                    "root";
+            private static final String PASSWORD =
+                    "root";
+
+
+            public static void createTableSatu() {
+                try {
+                   Connection conn = DriverManager.getConnection(DB_URL,USER,PASSWORD);
+                   Statement statement = conn.createStatement();
+                    String sql = " CREATE TABLE table_satu ( " +
+                            "   id_table_satu INT(11) not null PRIMARY KEY auto_increment, " +
+                            "   description VARCHAR(255) not null, " +
+                            "   create_user VARCHAR(255) not null, " +
+                            "   create_date DATE not null " +
+                            "   )";
+                    statement.executeUpdate(sql);
+                    System.out.println("INI CONNECTION SAYA : " + conn);
+                    System.out.println("CREATE TABLE SUCCESS ");
+                } catch (SQLException e) {
+                    System.out.println("GET CONNECTION FAILED");
+                    System.out.println("CREATE TABLE FAILED ");
+                    e.printStackTrace();
+                }
+            }
+
+            public static void createTableKaryawan(){
+
+                try {
+
+                    Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+                    Statement state = conn.createStatement();
+                    String CREATE = "CREATE TABLE KARYAWAN(" +
+                            "id_karyawan INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, " +
+                            "nama_karyawan VARCHAR(50) NOT NULL, " +
+                            "alamat_karyawan VARCHAR(255) NOT NULL, " +
+                            "telp_karyawan VARCHAR(14) NOT NULL " +
+                            ")";
+
+                    state.executeUpdate(CREATE);
+                    System.out.println("Ini Connection : " + conn);
+                    System.out.println("Create table success");
+
+                }catch (SQLException ex){
+                    Logger.getLogger(TransactionToJDBC.class.getName()).log(Level.SEVERE,null,ex);
+                }catch (Exception e){
+                    System.out.println("Failed create table ");
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
